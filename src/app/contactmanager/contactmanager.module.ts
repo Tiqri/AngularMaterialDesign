dcb5cde78984c093ede9188f1d6b4dc718acf3b7
Sameera <sda@tiqri.com>
2018-02-09 12:43:09 +0530

    added ngx charts support. sample chart added

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -7,12 +7,14 @@ import {FormsModule} from '@angular/forms';
 
 import { MaterialModule } from '../shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-  
+import { ChartsModule } from 'ng2-charts';
+
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { UserService } from './services/user.service';
 import { NewContactDialogComponent } from './components/new-contact-dialog/new-contact-dialog.component';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
 
 const routes : Routes = [
   {path: '', component: ContactmanagerAppComponent,
@@ -31,10 +33,11 @@ const routes : Routes = [
     HttpClientModule,
     MaterialModule,
     FlexLayoutModule,
+    ChartsModule,
     RouterModule.forChild(routes),
   ],
   providers:[UserService],
-  declarations: [ContactmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent, NewContactDialogComponent],
+  declarations: [ContactmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent, NewContactDialogComponent, LineChartComponent],
   entryComponents: [NewContactDialogComponent]
 })
 export class ContactmanagerModule { }
